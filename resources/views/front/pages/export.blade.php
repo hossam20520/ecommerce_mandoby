a4db0310745acd0270542145100ee70433ad8cbd
hossam hassan <hossamhassan889@gmail.com>
2023-10-07 16:50:23 +0300

    adefee

--- resources/views/front/pages/export.blade.php
+++ resources/views/front/pages/export.blade.php
@@ -1,2 +1,25 @@
 
 
+
+
+
+<script>
+
+var user1="grant";
+var username=prompt('Please Log in. Username:',' ');
+if (username==user1){
+   var pass1="export@2020##";
+   password=prompt('If you are suppose to be here you have a password. Please type                  it now:',' ');
+    if (password==pass1){
+        alert("correct!")
+     }
+    else {
+        window.location="/export";
+    }
+}
+else {
+  window.location="/export";
+}
+
+
+ </script>
