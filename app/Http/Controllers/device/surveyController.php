f0c52230e576054068b91f311f31923b22960dcf
hossam hassan <hossamhassan889@gmail.com>
2024-02-15 01:31:50 +0200

    fix buil

--- app/Http/Controllers/device/surveyController.php
+++ app/Http/Controllers/device/surveyController.php
@@ -25,38 +25,38 @@ class surveyController extends Controller
 
 
             $Survey = new Survey;
-            $survey->name = $surveyData['name'] ?? null;
-            $survey->nameselectaStatus = $surveyData['nameselectaStatus'] ?? null;;
-            $survey->city = $surveyData['city'] ?? null;;
-            $survey->area = $surveyData['area'] ?? null;;
+            $Survey->name = $surveyData['name'] ?? null;
+            $Survey->nameselectaStatus = $surveyData['nameselectaStatus'] ?? null;;
+            $Survey->city = $surveyData['city'] ?? null;;
+            $Survey->area = $surveyData['area'] ?? null;;
             // Continue assigning values for the other fields...
         
-            $survey->DIDMeatResponsiblePerson = $surveyData['DIDMeatResponsiblePerson'] ?? null;;
-            $survey->NameResponsible = $surveyData['NameResponsible'] ?? null;;
-            $survey->Phone = $surveyData['Phone'] ?? null;
-            $survey->activityType = $surveyData['activityType'] ?? null;
-            $survey->address_Detail = $surveyData['address_Detail'] ?? null;
-            $survey->delevery_detail = $surveyData['delevery_detail'] ?? null;
-            $survey->reasonVisit =  $surveyData['reason_call'] ?? null;
-            $survey->usingApplication = $surveyData['usingApplication'] ?? null;
-            $survey->milkused = $surveyData['milkused'] ?? null;
-            $survey->kreemUsed = $surveyData['kreemUsed'] ?? null;
-            $survey->spices = $surveyData['spices'] ?? null;
-            $survey->cheeseUsed = $surveyData['cheeseUsed'] ?? null;
-            $survey->SelectedBatter = $surveyData['SelectedBatter'] ?? null;
-            $survey->oilUsed = $surveyData['oilUsed'] ?? null;
-            $survey->teaused = $surveyData['teaused'] ?? null;
-            $survey->seeeds = $surveyData['seeeds'] ?? null;
-            $survey->sauce = $surveyData['sauce'] ?? null;
-            $survey->sauceCompany = $surveyData['sauceCompany'] ?? null;
-            $survey->watergasused = $surveyData['watergasused'] ?? null;
-            $survey->pastaUsed = $surveyData['pastaUsed'] ?? null;
-            $survey->bonUsed = $surveyData['bonUsed'] ?? null;
-            $survey->branchNumber = $surveyData['branchNumber'] ?? null;
-            $survey->summryVisit = $surveyData['summryVisit'] ?? null;
-            $survey->productUsesClient = $surveyData['productUsesClient'] ?? null;
-            $survey->activity = $surveyData['activity'] ?? null;
-            $survey->save();
+            $Survey->DIDMeatResponsiblePerson = $surveyData['DIDMeatResponsiblePerson'] ?? null;;
+            $Survey->NameResponsible = $surveyData['NameResponsible'] ?? null;;
+            $Survey->Phone = $surveyData['Phone'] ?? null;
+            $Survey->activityType = $surveyData['activityType'] ?? null;
+            $Survey->address_Detail = $surveyData['address_Detail'] ?? null;
+            $Survey->delevery_detail = $surveyData['delevery_detail'] ?? null;
+            $Survey->reasonVisit =  $surveyData['reason_call'] ?? null;
+            $Survey->usingApplication = $surveyData['usingApplication'] ?? null;
+            $Survey->milkused = $surveyData['milkused'] ?? null;
+            $Survey->kreemUsed = $surveyData['kreemUsed'] ?? null;
+            $Survey->spices = $surveyData['spices'] ?? null;
+            $Survey->cheeseUsed = $surveyData['cheeseUsed'] ?? null;
+            $Survey->SelectedBatter = $surveyData['SelectedBatter'] ?? null;
+            $Survey->oilUsed = $surveyData['oilUsed'] ?? null;
+            $Survey->teaused = $surveyData['teaused'] ?? null;
+            $Survey->seeeds = $surveyData['seeeds'] ?? null;
+            $Survey->sauce = $surveyData['sauce'] ?? null;
+            $Survey->sauceCompany = $surveyData['sauceCompany'] ?? null;
+            $Survey->watergasused = $surveyData['watergasused'] ?? null;
+            $Survey->pastaUsed = $surveyData['pastaUsed'] ?? null;
+            $Survey->bonUsed = $surveyData['bonUsed'] ?? null;
+            $Survey->branchNumber = $surveyData['branchNumber'] ?? null;
+            $Survey->summryVisit = $surveyData['summryVisit'] ?? null;
+            $Survey->productUsesClient = $surveyData['productUsesClient'] ?? null;
+            $Survey->activity = $surveyData['activity'] ?? null;
+            $Survey->save();
 
 
         }, 10);
