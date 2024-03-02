db30c265e52676e861a65fb04711de1921b7225d
hossam hassan <hossamhassan889@gmail.com>
2024-03-02 18:55:24 +0200

    aeere

--- app/Http/Controllers/device/AuthController.php
+++ app/Http/Controllers/device/AuthController.php
@@ -243,7 +243,7 @@ class AuthController extends Controller
     public function version(Request $request , $version){
  
     
-        return response()->json(['version' => '1.2' ,  'needupdate'=> false , 'message'=> "hello" , 'url' => 'url' ], 200);
+        return response()->json(['version' => '1.2' ,  'needupdate'=> true , 'message'=> "hello" , 'url' => 'url' ], 200);
     }
 
 
