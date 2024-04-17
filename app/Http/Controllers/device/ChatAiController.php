576b9c3b0bbf6c761e09599ae0258e81463faf88
hossam hassan <hossamhassan889@gmail.com>
2024-04-17 03:30:01 +0200

    faadd

--- app/Http/Controllers/device/ChatAiController.php
+++ app/Http/Controllers/device/ChatAiController.php
@@ -49,7 +49,7 @@ class ChatAiController extends Controller
             "top_p" => 1,
             "n" => 1,
             "stream" => false,
-            "max_tokens" => 250,
+            "max_tokens" => 9000,
             "presence_penalty" => 0,
             "frequency_penalty" => 0
         ];
